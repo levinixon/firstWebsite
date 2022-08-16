aee45d31bfed79ce3187c81530af7e95496d7d5d
levinixon <108950890+levinixon@users.noreply.github.com>
2022-08-16 19:46:56 +0100

    Update portfolio-3.js

--- portfolio-3.js
+++ portfolio-3.js
@@ -3,9 +3,9 @@
 
    const btns = document.querySelectorAll(".question-btn");
 
-   btns.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        console.log(e.currentTarget);
+   btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        console.log(e.currentTarget.parentElement);
     });
    });
 
